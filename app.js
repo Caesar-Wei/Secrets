71f46c807d3c312489a508f6e502f6a14c4076d0
Caesar-Wei <1229914919@qq.com>
2022-08-18 18:06:24 -0400

    use hash to encrypt

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const ejs = require('ejs');
 const mongoose = require('mongoose');
 const encrypt = require("mongoose-encryption");
 
+const md5 = require("md5");
 const app = express();
 
 app.use(express.static('public'));
@@ -46,7 +47,7 @@ app.get("/register", function(req, res) {
 app.post("/register", function(req, res) {
   const newUser = new User({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
 
   newUser.save(function(err) {
@@ -60,7 +61,7 @@ app.post("/register", function(req, res) {
 
 app.post("/login", function(req, res) {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({
     email: username
