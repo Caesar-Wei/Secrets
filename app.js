56844dda7cf1a7273a1a6a6c294778be1cbd8ae8
Caesar-Wei <1229914919@qq.com>
2022-08-19 07:17:16 -0400

    updated to salting

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require("mongoose-encryption");
 
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 const app = express();
 
 app.use(express.static('public'));
@@ -19,16 +20,11 @@ mongoose.connect("mongodb://localhost:27017/userDB", {
   useNewUrlParser: true
 });
 
-const userSchema = new mongoose.Schema ({
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
 
-
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET,
-  encryptedFields: ["password"]
-});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -45,23 +41,27 @@ app.get("/register", function(req, res) {
 });
 
 app.post("/register", function(req, res) {
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
-  });
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
 
-  newUser.save(function(err) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+
+    newUser.save(function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
   });
 });
 
 app.post("/login", function(req, res) {
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
+
 
   User.findOne({
     email: username
@@ -70,26 +70,17 @@ app.post("/login", function(req, res) {
       console.log(err);
     } else {
       if (foundUser) {
-        if (foundUser.password === password) {
-          res.render("secrets");
-        }
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if (result === true) {
+            res.render("secrets");
+          }
+        });
       }
-    }
-  })
-
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
-  });
-
-  newUser.save(function(err) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets");
     }
   });
 });
+
+
 
 
 app.listen(3000, function() {
